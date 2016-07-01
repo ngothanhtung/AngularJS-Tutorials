0dac20a9dbdc001b2adad5eb86ed90db527fb0bf
Ngo Thanh Tung <ngothanhtung.it@gmail.com>
2016-07-01 17:41:06 +0700

    Commit Chapter 01 - Homeworks

---
+++ Homeworks/chapter-01/data.js
@@ -0,0 +1,68 @@
+var products = [];
+products.push({
+    id: 1,
+    name: 'Product name 1',
+    imageUrl: 'images/1.jpg',
+    price: 100,
+    discount: 0,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    size: 'S / M / L / XL',
+    color: 'green, yellow, red',
+    stock: 10,
+    description: '',
+    moreImages: [{
+        caption: '',
+        imageUrl: ''
+    }]
+});
+
+products.push({
+    id: 2,
+    name: 'Product name 2',
+    imageUrl: 'images/2.jpg',
+    price: 100,
+    discount: 0,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    size: 'S / M / L / XL',
+    color: 'green, yellow, red',
+    stock: 10,
+    description: '',
+    moreImages: [{
+        caption: '',
+        imageUrl: ''
+    }]
+});
+
+products.push({
+    id: 3,
+    name: 'Product name 3',
+    imageUrl: 'images/3.jpg',
+    price: 100,
+    discount: 0,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    size: 'S / M / L / XL',
+    color: 'green, yellow, red',
+    stock: 10,
+    description: '',
+    moreImages: [{
+        caption: '',
+        imageUrl: ''
+    }]
+});
+
+products.push({
+    id: 4,
+    name: 'Product name 4',
+    imageUrl: 'images/4.jpg',
+    price: 100,
+    discount: 0,
+    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    size: 'S / M / L / XL',
+    color: 'green, yellow, red',
+    stock: 10,
+    description: '',
+    moreImages: [{
+        caption: '',
+        imageUrl: ''
+    }]
+});
