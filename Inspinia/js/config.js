e25a97223ee6e2c16e89da64d867200a5c17d7d2
Ngo Thanh Tung <ngothanhtung.it@gmail.com>
2016-07-02 08:52:48 +0700

    Commit Update Inspinia

---
+++ Inspinia/js/config.js
@@ -0,0 +1,51 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+var siteName = 'ASP.NET VIETNAM';
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
+    $urlRouterProvider.otherwise("/index/main");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('index', {
+            abstract: true,
+            url: "/index",
+            templateUrl: "views/common/content.html",
+        })
+        .state('index.main', {
+            url: "/main",
+            templateUrl: "views/main.html",
+            data: { pageTitle: 'Home' }
+        })
+        .state('index.minor', {
+            url: "/minor",
+            templateUrl: "views/minor.html",
+            data: { pageTitle: 'Example view minor' }
+        })
+        .state('index.three', {
+            url: "/three",
+            templateUrl: "views/three.html",
+            data: { pageTitle: 'Example view 3' }
+        })
+        .state('index.four', {
+            url: "/three",
+            templateUrl: "views/four.html",
+            data: { pageTitle: 'Example view 4' }
+        })
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
